44307f0cee61ed7f11595512a09d78d3f3840359
Kenton Troy <kentontroy@yahoo.com>
2021-11-28 01:47:57 -0500

    Add files via upload

---
+++ scripts/sim.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -
+
+################################################################################
+# This is the script that allows you to start or stop sim.
+# Usage: ./sim.sh start | stop | status
+
+SIM_LOG=${SIM_HOME}/logs/sim.log
+SIM_PID=sim.pid
+
+function usage() {
+  printf "Usage: %s start | stop | status\n" $0
+}
+
+function start_sim() {
+  nohup python3 ${SIM_HOME}/src/simMain.py &> $1 &
+  echo $! > $SIM_PID
+}
+
+function test_mods() {
+  python3 ${SIM_HOME}/src/simTest.py 
+}
+
+function stop_sim() {
+# try to find the PID of sim process and kill it
+  if [ -f $SIM_PID ]; then
+    if kill -0 `cat $SIM_PID` > /dev/null 2>&1; then
+      echo 'Shutting down sim ...'
+      kill `cat $SIM_PID`
+      rm $SIM_PID
+    fi
+  fi
+	
+# ... as well as clean up the nohup stuff
+  if [ -f nohup.out ]; then
+    rm nohup.out
+  fi
+}
+
+# main script
+case $1 in
+ start )  start_sim $SIM_LOG ;;
+ stop )   stop_sim ;;
+ test )   test_mods ;;
+ * )      usage ; exit 1 ; ;;
+esac
